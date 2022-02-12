6061436cbcbf356c94cd34f3ebbbeda8f6f2d56f
Lewandy Dilone <lewandydilone1@live.com>
2022-02-12 17:06:41 -0400

    chore: remove vetur config file

--- vetur.config.js
+++
@@ -1,9 +0,0 @@
-// vetur.config.js
-/** @type {import('vls').VeturConfig} */
-module.exports = {
-  // **optional** default: `[{ root: './' }]`
-  // support monorepos
-  projects: [
-    './src', // Shorthand for specifying only the project root location
-  ]
-}
