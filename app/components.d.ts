7162efc86818837f22151758bef11332eb5f3861
Lewandy Dilone <lewandydilone1@live.com>
2022-03-05 23:48:40 -0400

    wip: main page

--- app/components.d.ts
+++ app/components.d.ts
@@ -2,20 +2,21 @@
 // We suggest you to commit this file into source control
 // Read more: https://github.com/vuejs/vue-next/pull/3399
 
-declare module 'vue' {
+declare module "vue" {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElContainer: typeof import('element-plus/es')['ElContainer']
-    ElFooter: typeof import('element-plus/es')['ElFooter']
-    ElHeader: typeof import('element-plus/es')['ElHeader']
-    ElIcon: typeof import('element-plus/es')['ElIcon']
-    ElMain: typeof import('element-plus/es')['ElMain']
-    ElMenu: typeof import('element-plus/es')['ElMenu']
-    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
-    ElSpace: typeof import('element-plus/es')['ElSpace']
-    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
-    TheHeader: typeof import('./src/components/TheHeader.vue')['default']
+    ElButton: typeof import("element-plus/es")["ElButton"];
+    ElCol: typeof import("element-plus/es")["ElCol"];
+    ElContainer: typeof import("element-plus/es")["ElContainer"];
+    ElFooter: typeof import("element-plus/es")["ElFooter"];
+    ElHeader: typeof import("element-plus/es")["ElHeader"];
+    ElIcon: typeof import("element-plus/es")["ElIcon"];
+    ElMain: typeof import("element-plus/es")["ElMain"];
+    ElMenu: typeof import("element-plus/es")["ElMenu"];
+    ElMenuItem: typeof import("element-plus/es")["ElMenuItem"];
+    ElRow: typeof import("element-plus/es")["ElRow"];
+    ElSpace: typeof import("element-plus/es")["ElSpace"];
+    ElSubMenu: typeof import("element-plus/es")["ElSubMenu"];
   }
 }
 
-export { }
+export {};
--- app/src/pages/Contracts.vue
+++ app/src/pages/Contracts.vue
@@ -1,17 +1,30 @@
 <template>
-  <el-container class="contracts__container">Contracts page</el-container>
+  <el-row justify="end">
+    <el-col :span="4">
+      <el-button type="primary">
+        Upload
+        <el-icon class="el-icon--right">
+          <Upload />
+        </el-icon>
+      </el-button>
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="24">Cards</el-col>
+  </el-row>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue"
+import { Upload } from "@element-plus/icons-vue"
 
 export default defineComponent({
   name: "Contracts",
+  components: {
+    Upload
+  }
 })
 </script>
 
 <style scoped>
-.contracts__container {
-  height: 100%;
-}
 </style>
